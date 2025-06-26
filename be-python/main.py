ccde7423ecdd6954fc04ff5dad83686d47ded78b
Luis Maiworm <luis.maiworm@outlook.de>
2025-06-26 15:53:32 +0200

    feat(be-python): capsulate router, add custom prometheus metrics to endpoints

--- be-python/main.py
+++ be-python/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Response
 from database import init_db
 from router import router
-from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 
 @asynccontextmanager
@@ -14,18 +14,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-REQUEST_COUNTER = Counter(
-    "python_http_requests_total",
-    "Total number of requests",
-    ["method", "endpoint", "status_code"]
-)
+
 
 app.include_router(router)
-
-@app.get("/test")
-def test_endpoint():
-    REQUEST_COUNTER.labels(method="GET", endpoint="/test", status_code=200).inc()
-    return {"message": "Hello, World!"}
 
 @app.get("/metrics")
 def metrics():
--- be-python/router.py
+++ be-python/router.py
@@ -2,38 +2,54 @@ from fastapi import APIRouter, Depends
 from schemas import UserSchema
 from models import UserModel
 from repository import PythonRepository, get_python_repository
+from prometheus_client import Counter
 
 router = APIRouter()
+
+REQUEST_COUNTER = Counter(
+    "python_http_requests_total",
+    "Total number of requests",
+    ["method", "endpoint", "status_code"]
+)
 
 @router.get("/users/{user_id}")
 def get_user(user_id: int, repository: PythonRepository = Depends(get_python_repository)):
     user = repository.read(user_id)
     if user:
+        REQUEST_COUNTER.labels(method='GET', endpoint='/users/{user_id}', status_code=200).inc()
         return user
+    REQUEST_COUNTER.labels(method='GET', endpoint='/users/{user_id}', status_code=404).inc()
     return {"message": "User not found"}, 404
 
 @router.get("/users/")
 def get_all_user(repository: PythonRepository = Depends(get_python_repository)):
     user = repository.read_all()
     if user:
+        REQUEST_COUNTER.labels(method='GET', endpoint='/users/', status_code=200).inc()
         return user
+    REQUEST_COUNTER.labels(method='GET', endpoint='/users/', status_code=404).inc()
     return {"message": "User not found"}, 404
 
 @router.post("/users/")
 def create_user(user: UserSchema, repository: PythonRepository = Depends(get_python_repository)):
     db_user = UserModel(**user.model_dump())
     new_user = repository.create(db_user)
+    REQUEST_COUNTER.labels(method="POST", endpoint="/users/", status_code=201).inc()
     return new_user
 
 @router.put("/users/{user_id}")
 def update_user(user_id: int, user: UserSchema, repository: PythonRepository = Depends(get_python_repository)):
     updated_user = repository.update(user_id, **user.model_dump())
     if updated_user:
+        REQUEST_COUNTER.labels(method="PUT", endpoint="/users/", status_code=200).inc()
         return updated_user
+    REQUEST_COUNTER.labels(method="PUT", endpoint="/users/", status_code=404).inc()
     return {"message": "User not found"}, 404
 
 @router.delete("/users/{user_id}")
 def delete_user(user_id: int, repository: PythonRepository = Depends(get_python_repository)):
     if repository.delete(user_id):
+        REQUEST_COUNTER.labels(method="DELETE", endpoint="/users/", status_code=200).inc()
         return {"message": "User deleted"}
+    REQUEST_COUNTER.labels(method="DELETE", endpoint="/users/", status_code=404).inc()
     return {"message": "User not found"}, 404
