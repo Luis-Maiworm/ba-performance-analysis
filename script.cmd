a181d355f4376664667d7d2bd69fea8306ec9977
Luis Maiworm <luis.maiworm@outlook.de>
2025-07-11 09:02:09 +0200

    feat(app): adjusted .cmd script file

--- script.cmd
+++ script.cmd
@@ -3,10 +3,17 @@ echo Starte Ablauf %cd%
 date /T
 time /T
 
-for /L %%i in (1,1,5) do (
-    echo Durchlauf %%i
-    docker compose run --rm -e SERVICE=node k6 run --out experimental-prometheus-rw=%K6_PROMETHEUS_RW_SERVER_URL% /k6/tests/rampLoad.js
-    timeout /t 10 >nul
-)
+set ARG1=%1
+set TEST=test
+set TEST2=rampLoad
+
+@REM docker compose run --rm -e SERVICE=%ARG1% -e K6_PROMETHEUS_REMOTE_WRITE_LABELS="job=be_$ARG1" k6 run --out experimental-prometheus-rw=%K6_PROMETHEUS_RW_SERVER_URL% /k6/tests/%TEST%.js
+docker compose run --rm -e SERVICE=%ARG1% k6 run --out experimental-prometheus-rw=%K6_PROMETHEUS_RW_SERVER_URL% /k6/tests/%TEST%.js
+
+@REM for /L %%i in (1,1,5) do (
+@REM     echo Durchlauf %%i
+@REM     docker compose run --rm -e SERVICE=%ARG1% k6 run --out experimental-prometheus-rw=%K6_PROMETHEUS_RW_SERVER_URL% /k6/tests/%TEST%.js
+@REM     timeout /t 10 >nul
+@REM )
 
 echo Fertig
