04bb4c2c4a72ea92158f0bfd8236492e1dc0685c
Luis Maiworm <luis.maiworm@outlook.de>
2025-07-18 14:29:13 +0200

    feat(app): unified shutting down containers, removed redundant init build of FS

--- run.cmd
+++ run.cmd
@@ -29,14 +29,14 @@ docker compose down -v --remove-orphans %LANG%
 docker compose up -d --build %LANG%
 echo Docker Setup finished
 
+timeout /t 10 >nul
 
 echo Starting %TEST_TYPE%.js with %LANG%.
 
-docker compose --profile tests run --rm -e SERVICE=%LANG% k6 run --tag testid=%date%_%time%_%LANG% --out experimental-prometheus-rw=%K6_PROMETHEUS_RW_SERVER_URL% /k6/tests/%TEST_TYPE%.js
+docker compose --profile tests run --rm -e SERVICE=%LANG% k6 run --tag testid=%date%_%time%_%LANG% --out experimental-prometheus-rw /k6/tests/%TEST_TYPE%.js
 
 echo Test finished, shutting down %LANG% process.
-docker compose stop %LANG%
-docker compose rm -f %LANG%
+docker compose down -v --remove-orphans %LANG%
 echo %LANG% stopped and removed.
 
 
